0f1220f63314581e3be06eb0c0788633b78b800b
Robert Collins <beaucollins@gmail.com>
2016-03-04 17:21:42 -0800

    customer service namespace

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Tinkerchat Chat Server
 
 Socket.IO based chat server for tinkerchat.
+
+In progress implementation of [Tinkerchat Service](/automattic/tinkerchat/SERVICE.md)
--- test/lib/customer-test.js
+++ test/lib/customer-test.js
@@ -3,9 +3,33 @@ import { ok, equal } from 'assert'
 import { EventEmitter } from 'events'
 
 describe( 'customer service', () => {
-	let io
+	let server, socket, client
+
 	beforeEach( () => {
-		io = new EventEmitter()
+		server = new EventEmitter()
+		socket = new EventEmitter()
+		client = new EventEmitter()
+		let emitClient = client.emit.bind( client )
+		let emitSocket = socket.emit.bind( socket )
+		socket.emit = emitClient
+		client.emit = emitSocket
+	} )
+
+	describe( 'with authorized user', () => {
+		beforeEach( ( next ) => {
+			client.on( 'init', () => next() )
+			customer( server, ( token, callback ) => callback( null, { id: 'abdefgh' } ) )
+			server.emit( 'connection', socket )
+			client.emit( 'token', 'hello' )
+		} )
+
+		it( 'should receive message and broadcast it', ( done ) => {
+			client.once( 'message', ( message ) => {
+				equal( message, 'hello world' )
+				done()
+			} )
+			client.emit( 'message', 'hello world' )
+		} )
 	} )
 
 	it( 'should allow connections', () => {
@@ -16,5 +40,33 @@ describe( 'customer service', () => {
 			connected = true
 		}} )
 		ok( connected )
+	} )
+
+	it( 'should request a token', ( done ) => {
+		client.on( 'token', () => done() )
+
+		customer( server )
+		server.emit( 'connection', socket )
+	} )
+
+	it( 'should authenticate and init client', ( done ) => {
+		customer( server, ( token, callback ) => {
+			equal( typeof( callback ), 'function' )
+			equal( token, 'valid' )
+			client.once( 'init', () => done() )
+			callback( null, { id: 'user1' } )
+		} )
+
+		server.emit( 'connection', socket )
+		client.emit( 'token', 'valid' )
+	} )
+
+	it( 'should fail to authenticate with invalid token', ( done ) => {
+		customer( server, ( token, callback ) => {
+			client.once( 'unauthorized', () => done() )
+			callback( new Error( 'Invalid token' ) )
+		} )
+		server.emit( 'connection', socket )
+		client.emit( 'token', 'invalid' )
 	} )
 } )
