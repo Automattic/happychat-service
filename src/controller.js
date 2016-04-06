d83b54306170f783d6802f68923d3abba6f814cd
Robert Collins <beaucollins@gmail.com>
2016-04-06 09:23:37 -0700

    Integrating chatlist with controller

--- src/controller.js
+++ src/controller.js
@@ -1,3 +1,7 @@
+import { isFunction } from 'lodash/lang'
+
+import { ChatList } from './chat-list'
+
 const debug = require( 'debug' )( 'tinkerchat:controller' )
 
 // change a lib/customer message to what an agent client expects
@@ -8,26 +12,45 @@ const formatAgentMessage = ( author_type, author_id, context, { id, timestamp, t
 	author_type
 } )
 
-const forward = ( dest ) => ( org, event, dstEvent ) => {
-	org.on( event, ( ... args ) => dest.emit( dstEvent ? dstEvent : event, ... args ) )
+const pure = ( ... args ) => args
+
+const forward = ( dest ) => ( org, event, dstEvent, mapArgs = pure ) => {
+	if ( isFunction( dstEvent ) ) {
+		mapArgs = dstEvent
+		dstEvent = event
+	}
+	if ( !dstEvent ) {
+		dstEvent = event
+	}
+	org.on( event, ( ... args ) => dest.emit( dstEvent, ... mapArgs( ... args ) ) )
 }
 
 const on = ( emitter, event, listener ) => emitter.on( event, listener )
 
-export default ( { customers, agents } ) => {
+export default ( { customers, agents, operators } ) => {
 	const toAgents = forward( agents )
+	const chats = new ChatList( { customers, operators } )
 
-	on( customers, 'message', ( { id }, message ) => {
-		debug( 'received customer message', message )
-		agents.emit( 'receive', formatAgentMessage( 'customer', id, id, message ) )
+	chats
+	.on( 'miss', ( e, { id } ) => {
+		debug( 'failed to find operator', e, id )
+	} )
+	.on( 'open', ( { id } ) => {
+		debug( 'looking for operator', id )
+	} )
+	.on( 'found', ( channel, operator ) => {
+		debug( 'found operator', channel.id, operator.id )
 	} )
 
 	// forward customer join and leave events to agents
+	toAgents( customers, 'message', 'receive', ( { id }, message ) => {
+		return [ formatAgentMessage( 'customer', id, id, message ) ]
+	} )
 	toAgents( customers, 'join', 'customer.join' )
 	toAgents( customers, 'leave', 'customer.leave' )
 
 	on( agents, 'message', ( message ) => {
-		debug( 'received agent message', message )
 		customers.emit( 'receive', Object.assign( {}, { author_type: 'agent' }, message ) )
 	} )
 }
+
