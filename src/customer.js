b0fd3332c938b3c3fea36e939fbb39bceca8c36a
Robert Collins <beaucollins@gmail.com>
2016-04-06 22:32:26 -0700

    fixing bug with message context

--- src/customer.js
+++ src/customer.js
@@ -46,9 +46,8 @@ export default ( io ) => {
 	events.io = io
 
 	events.on( 'receive', ( user, message ) => {
-		let { context } = message
-		debug( 'sendding message to customer', user, context, message )
-		io.to( context ).emit( 'message', message )
+		debug( 'sendding message to customer', user, message )
+		io.to( user.id ).emit( 'message', message )
 	} )
 	io.on( 'connection', ( socket ) => {
 		debug( 'customer connecting' )
