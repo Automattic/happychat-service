0592ecd79046421483db778ee58ed4283d7b8e33
Robert Collins <beaucollins@gmail.com>
2016-04-25 11:26:38 -0700

    Refactoring chat assignment code and updating tests

--- src/operator.js
+++ src/operator.js
@@ -2,7 +2,9 @@ import EventEmitter from 'events'
 import { onConnection, timestamp } from './util'
 import { parallel } from 'async'
 import { isEmpty } from 'lodash/lang'
-import { assign } from 'lodash/object'
+import { set, assign, values } from 'lodash/object'
+import { throttle } from 'lodash/function'
+import { map, forEach, reduce } from 'lodash/collection'
 
 const DEFAULT_TIMEOUT = 1000
 
@@ -21,39 +23,156 @@ const identityForUser = ( { id, displayName, avatarURL } ) => (
 	{ id, displayName, avatarURL }
 )
 
+const queryClients = ( io, room ) => new Promise( ( resolve, reject ) => {
+	io.in( room ).clients( ( error, clients ) => {
+		if ( error ) {
+			return reject( error )
+		}
+		resolve( clients )
+	} )
+} )
+
+const online = ( io ) => queryClients( io, 'online' )
+
+const queryAvailability = ( chat, clients, io ) => new Promise( ( resolve, reject ) => {
+	if ( isEmpty( clients ) ) {
+		return reject( new Error( 'no operators connected' ) )
+	}
+
+	parallel( clients.map( ( socket_id ) => ( complete ) => {
+		withTimeout( ( cancel ) => {
+			const socket = io.connected[socket_id]
+			socket.emit( 'available', chat, ( available ) => {
+				complete( null, assign( { socket }, available ) )
+				cancel()
+			} )
+		}, () => complete( null, { capacity: 0, load: 0 } ) )
+	} ), ( error, results ) => {
+		if ( error ) {
+			return reject( error )
+		}
+		resolve( results )
+	} )
+} )
+
+const pickAvailable = ( availability ) => new Promise( ( resolve, reject ) => {
+	const operator = availability
+	.filter( ( op ) => op.capacity - op.load > 0 )
+	.sort( ( a, b ) => {
+		const a_open = a.capacity - a.load
+		const b_open = b.capacity - b.load
+		if ( a_open === b_open ) return 0
+		return ( a_open > b_open ? -1 : 1 )
+	} )
+	.sort( ( a, b ) => {
+		if ( a.load === b.load ) {
+			return 0
+		}
+		return a.load < b.load ? -1 : 1
+	} )[0]
+
+	if ( !operator ) {
+		return reject( new Error( 'no operators available' ) )
+	}
+
+	if ( !operator.id ) {
+		return reject( new Error( 'invalid operator' ) )
+	}
+
+	resolve( operator )
+} )
+
+const queryOnline = ( io, timeout ) => online( io ).then( ( clients ) => new Promise( ( resolve, reject ) => {
+	parallel( map( clients, ( client_id ) => ( complete ) => {
+		const client = io.connected[client_id]
+		withTimeout( ( cancel ) => {
+			client.emit( 'identify', ( identity ) => {
+				cancel()
+				complete( null, identity )
+			} )
+		}, () => complete( null, null ), timeout )
+	} ), ( error, results ) => {
+		if ( error ) {
+			return reject( error )
+		}
+		resolve( results )
+	} )
+} ) )
+
+const reduceUniqueOperators = ( operators ) => values( reduce( operators, ( unique, operator ) => {
+	if ( isEmpty( operator ) ) {
+		return unique
+	}
+	return set( unique, operator.id, operator )
+}, {} ) )
+
+const all = ( ... fns ) => ( ... args ) => forEach( fns, ( fn ) => fn( ... args ) )
+
+const emitOnline = throttle( ( io ) => {
+	// when a socket disconnects, query the online room for connected operators
+	queryOnline( io ).then( ( identities ) => {
+		debug( 'emitOnline called', identities )
+		io.emit( 'operators.online', reduceUniqueOperators( identities ) )
+	} )
+	.catch( ( e ) => debug( 'failed to query online', e ) )
+}, 100 )
+
 const join = ( { socket, events, user, io } ) => {
-	// TODO: initialize the agent
 	const user_room = `operators/${user.id}`
 	debug( 'initialize the operator', user )
+
 	socket.on( 'status', ( status, done ) => {
-		debug( 'set operator status', status )
+		// TODO: if operator has multiple clients, move all of them?
 		if ( status === 'online' ) {
 			debug( 'joining room', 'online' )
-			socket.join( 'online', done )
+			socket.join( 'online', all( done, () => emitOnline( io ) ) )
 		} else {
-			socket.leave( 'online', done )
+			socket.leave( 'online', all( done, () => emitOnline( io ) ) )
 		}
 	} )
 
+	socket.on( 'disconnect', () => emitOnline() )
+
 	socket.join( user_room, () => {
 		socket.emit( 'init', user )
-		events.emit( 'init', user )
+		events.emit( 'init', { user, socket, room: user_room } )
 	} )
 
 	socket.on( 'message', ( chat_id, { id, text } ) => {
 		const meta = {}
 		const userIdentity = identityForUser( user )
-		debug( 'Identify user', user )
 		const message = { id: id, text, timestamp: timestamp(), user: userIdentity, meta }
 		// all customer connections for this user receive the message
 		debug( 'broadcasting message', user.id, id, message )
 		events.emit( 'message', { id: chat_id }, user, message )
 	} )
-
-	socket.on( 'disconnect', () => {
-		debug( 'goodbye!', socket.rooms )
-	} )
 }
+
+const operatorClients = ( { io, operator } ) => new Promise( ( resolve, reject ) => {
+	const room = `operators/${ operator.id }`
+	io.in( room ).clients( ( error, clients ) => {
+		if ( error ) reject( error )
+		resolve( map( clients ), ( socketid ) => io.connected[socketid] )
+	} )
+} )
+
+const assignChat = ( { io, operator, chat, room } ) => new Promise( ( resolve, reject ) => {
+	const operator_room_name = `operators/${operator.id}`
+	// send the event to the operator and confirm that the chat was opened
+	// TODO: timeouts? only one should have to succeed or should all of them have to succeed?
+	operatorClients( { io, operator } )
+	.then( ( clients ) => {
+		parallel( clients.map( ( socketid ) => ( complete ) => {
+			io.connected[socketid].join( room, complete )
+		} ), ( e ) => {
+			if ( e ) {
+				reject( e )
+			}
+			io.in( operator_room_name ).emit( 'chat.open', chat )
+			resolve( operator )
+		} )
+	} )
+} )
 
 export default ( io ) => {
 	const events = new EventEmitter()
@@ -65,76 +184,28 @@ export default ( io ) => {
 		io.in( room_name ).emit( 'chat.message', { id }, message )
 	} )
 
-	events.on( 'assign', ( chat, callback ) => {
+	events.on( 'recover', ( { user }, chats, callback ) => {
+		parallel( map( chats, ( chat ) => ( complete ) => {
+			const room = `customers/${ chat.id }`
+			assignChat( { io, operator: user, chat, room } ).then( () => complete( null ), complete )
+		} ), ( e ) => {
+			if ( e ) {
+				debug( 'failed to recover chats', e )
+				return
+			}
+			callback()
+		} )
+	} )
+
+	events.on( 'assign', ( chat, room, callback ) => {
 		// find an operator
 		debug( 'find an operator for', chat.id )
-		const room_name = `customers/${ chat.id }`
-		io.in( 'online' ).clients( ( error, clients ) => {
-			if ( error ) {
-				return callback( error )
-			}
-			debug( 'online: ', clients.length )
-
-			const ifNotAvailable = () => callback( new Error( 'no clients available' ) )
-			// if there are no clients available emit error
-			if ( isEmpty( clients ) ) {
-				return ifNotAvailable()
-			}
-
-			// iterate each socket and see if there are any available?
-			parallel( clients.map( ( socket_id ) => ( complete ) => {
-				const socket = io.connected[socket_id]
-				debug( 'asking', socket_id )
-				withTimeout( ( cancel ) => {
-					socket.emit( 'available', chat, ( available ) => {
-						complete( null, assign( { socket }, available ) )
-						cancel()
-					} )
-				}, () => complete( null, { capacity: 0, load: 0 } ) )
-			} ), ( error, results ) => {
-				if ( error ) {
-					return ifNotAvailable()
-				}
-				if ( isEmpty( results ) ) {
-					return ifNotAvailable()
-				}
-
-				// pick the first socket with the most space and least load
-				const operator = results
-				.filter( ( op ) => op.capacity - op.load > 0 )
-				.sort( ( a, b ) => {
-					const a_open = a.capacity - a.load
-					const b_open = b.capacity - b.load
-					if ( a_open === b_open ) return 0
-					return ( a_open > b_open ? -1 : 1 )
-				} )
-				.sort( ( a, b ) => {
-					if ( a.load === b.load ) {
-						return 0
-					}
-					return a.load < b.load ? -1 : 1
-				} )[0]
-
-				if ( !operator ) {
-					return ifNotAvailable()
-				}
-
-				if ( !operator.id ) {
-					return ifNotAvailable()
-				}
-
-				const operator_room_name = `operators/${operator.id}`
-				// send the event to the operator and confirm that the chat was opened
-				io.in( operator_room_name ).clients( ( error, clients ) => {
-					parallel( clients.map( ( socketid ) => ( complete ) => {
-						io.connected[socketid].join( room_name, complete )
-					} ), ( e ) => {
-						io.in( operator_room_name ).emit( 'chat.open', chat )
-						callback( null, operator )
-					} )
-				} )
-			} )
-		} )
+		online( io )
+		.then( ( clients ) => queryAvailability( chat, clients, io ) )
+		.then( pickAvailable )
+		.then( ( operator ) => assignChat( { io, operator, chat, room } ) )
+		.then( ( operator ) => callback( null, operator ) )
+		.catch( callback )
 	} )
 
 	io.on( 'connection', ( socket ) => {
--- test/unit/operator-test.js
+++ test/unit/operator-test.js
@@ -1,6 +1,9 @@
 import { ok, equal, deepEqual } from 'assert'
 import operator from '../../src/operator'
 import mockio from '../mock-io'
+import { tick } from '../tick'
+import { map } from 'lodash/collection'
+import { isEmpty } from 'lodash/lang'
 
 const debug = require( 'debug' )( 'tinkerchat:test:operators' )
 
@@ -9,32 +12,45 @@ describe( 'Operators', () => {
 	let socketid = 'socket-id'
 	let user
 	let socket, client, server
+
+	const connectOperator = ( { socket: useSocket, client: useClient }, authUser = { id: 'user-id', displayName: 'name' } ) => new Promise( ( resolve ) => {
+		debug( 'connect operator', authUser )
+		operators.once( 'connection', ( _, callback ) => callback( null, authUser ) )
+		useClient.once( 'init', ( clientUser ) => {
+			useClient.emit( 'status', 'online', () => resolve( clientUser ) )
+		} )
+		server.connect( useSocket )
+	} )
+
 	beforeEach( () => {
 		( { socket, client, server } = mockio( socketid ) )
 		operators = operator( server )
 	} )
 
 	describe( 'when authenticated and online', () => {
+		var op = { id: 'user-id', displayName: 'furiosa', avatarURL: 'url', priv: 'var' }
 		beforeEach( ( done ) => {
-			operators.on( 'connection', ( _, callback ) => {
-				callback( null, { id: 'user-id', displayName: 'furiosa', avatarURL: 'url', priv: 'var' } )
+			connectOperator( { socket, client }, op )
+			.then( ( operatorUser ) => {
+				user = operatorUser
+				done()
 			} )
-			client.once( 'init', ( clientUser ) => {
-				user = clientUser
-				client.emit( 'status', 'online', () => {
-					done()
-				} )
-			} )
-			server.connect( socket )
+		} )
+
+		it( 'should recover chats for an operator', ( done ) => {
+			operators.emit( 'recover', { user: op }, [ { id: 'something' } ], tick( () => {
+				equal( operators.io.rooms['customers/something'].length, 1 )
+				done()
+			} ) )
 		} )
 
 		it( 'should emit message', ( done ) => {
-			operators.on( 'message', ( { id: chat_id }, { id, displayName, avatarURL, priv }, { text, user } ) => {
+			operators.on( 'message', ( { id: chat_id }, { id, displayName, avatarURL, priv }, { text, user: author } ) => {
 				ok( id )
 				ok( displayName )
 				ok( avatarURL )
 				ok( priv )
-				ok( ! user.priv )
+				ok( ! author.priv )
 				equal( chat_id, 'chat-id' )
 				equal( text, 'message' )
 				done()
@@ -44,7 +60,10 @@ describe( 'Operators', () => {
 
 		it( 'should assign an operator to a new chat', ( done ) => {
 			// set up a second client
-			const { client: clientb } = server.connectNewClient( 'client-b', () => {
+			const connection = server.newClient()
+			const { client: clientb } = connection
+			connectOperator( connection, user )
+			.then( ( userb ) => {
 				let a_open = false, b_open = false;
 				client.on( 'chat.open', () => {
 					a_open = true
@@ -53,30 +72,55 @@ describe( 'Operators', () => {
 					b_open = true
 				} )
 
-				clientb.once( 'init', ( userb ) => {
-					clientb.emit( 'status', 'online', () => {
-						client.on( 'available', ( chat, callback ) => {
-							equal( chat.id, 'chat-id' )
-							callback( { load: 5, capacity: 6, id: user.id } )
-						} )
-						clientb.on( 'available', ( chat, callback ) => {
-							callback( { load: 5, capacity: 5, id: userb.id } )
-						} )
-						operators.emit( 'assign', { id: 'chat-id' }, ( error, op ) => {
-							ok( ! error )
-							ok( a_open )
-							ok( b_open )
-							ok( op.socket )
-							equal( op.id, 'user-id' )
-							done()
-						} )
-					} )
+				client.on( 'available', ( chat, callback ) => {
+					equal( chat.id, 'chat-id' )
+					callback( { load: 5, capacity: 6, id: user.id } )
+				} )
+				clientb.on( 'available', ( chat, callback ) => {
+					callback( { load: 5, capacity: 5, id: userb.id } )
+				} )
+				operators.emit( 'assign', { id: 'chat-id' }, 'room-name', ( error, assigned ) => {
+					ok( ! error )
+					ok( a_open )
+					ok( b_open )
+					ok( assigned.socket )
+					equal( assigned.id, 'user-id' )
+					done()
 				} )
 			} )
 		} )
+
+		it( 'should notify with updated operator list when operator joins', ( done ) => {
+			const userb = { id: 'a-user', displayName: 'Jem' }
+			const userc = { id: 'abcdefg', displayName: 'other' }
+			server.on( 'operators.online', tick( ( identities ) => {
+				equal( identities.length, 3 )
+				deepEqual( map( identities, ( { displayName } ) => displayName ), [ 'furiosa', 'Jem', 'other' ] )
+				done()
+			} ) )
+
+			const connectiona = server.newClient()
+			const connectionb = server.newClient()
+			const connectionc = server.newClient()
+
+			client.on( 'identify', ( callback ) => callback( user ) )
+			connectiona.client.on( 'identify', ( callback ) => callback( userb ) )
+			connectionb.client.on( 'identify', ( callback ) => callback( user ) )
+			connectionc.client.on( 'identify', ( callback ) => callback( userc ) )
+			connectOperator( connectiona, userb )
+			.then( () => connectOperator( connectionb, user ) )
+			.then( () => connectOperator( connectionc, userc ) )
+		} )
 	} )
 
-	it( 'should fail', () => {
-		ok( operators )
+	it( 'should send init message to events', ( done ) => {
+		operators.on( 'init', ( { user: u, socket: s, room } ) => {
+			ok( u )
+			ok( s )
+			ok( room )
+			equal( room, `operators/${u.id}` )
+			done()
+		} )
+		connectOperator( server.newClient(), { id: 'a-user' } ).catch( done )
 	} )
 } )
