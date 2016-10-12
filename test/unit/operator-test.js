390554a01298dd28245d231a0cf9b676427b20a0
Robert Collins <robert@automattic.com>
2016-10-12 09:29:22 -0700

    Fixes code style issues

--- test/unit/operator-test.js
+++ test/unit/operator-test.js
@@ -69,9 +69,9 @@ describe( 'Operators', () => {
 		} )
 
 		it( 'should handle `chat.typing` from client and pass to events', ( done ) => {
-			operators.on( 'typing', ( chat, user, text ) => {
+			operators.on( 'typing', ( chat, typingUser, text ) => {
 				equal( chat.id, 'chat-id' )
-				equal( user.id, op.id )
+				equal( typingUser.id, op.id )
 				equal( text, 'typing a message...' )
 				done()
 			} )
