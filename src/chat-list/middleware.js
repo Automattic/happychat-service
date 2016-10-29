e28b73e4b7189315c845646e1e1e1fd2cc4afb6c
Robert Collins <robert@automattic.com>
2016-10-28 21:58:04 -0700

    Refactors messy switch statement

--- src/chat-list/middleware.js
+++ src/chat-list/middleware.js
@@ -168,120 +168,157 @@ export default ( { customers, operators, events } ) => store => {
 		store.dispatch( closeChat( chat_id, operator ) )
 	} )
 
+	const handleReceiveCustomerMessage = ( action ) => {
+		debug( 'see if we should assign?', getChatStatus( action.chat.id, store.getState() ) )
+		if ( isChatStatusNew( action.chat.id, store.getState() ) ) {
+			store.dispatch( insertPendingChat( action.chat ) )
+			return
+		}
+		// TODO: check if there is an operator in the room
+		debug( 'chat exists time to make sure someone is home' )
+	}
+
+	const handleSetChatMissed = ( action, lastState ) => {
+		let previousStatus = getChatStatus( action.chat_id, lastState )
+		let missedChat = getChat( action.chat_id, store.getState() )
+		if ( previousStatus !== STATUS_MISSED ) {
+			debug( 'chat missed', action.chat_id, previousStatus, action.error )
+			events.emit( 'miss', action.error, missedChat, previousStatus )
+		}
+	}
+
+	const handleCloseChat = ( action, lastState ) => {
+		let chat = getChat( action.chat_id, lastState )
+		const room_name = `customers/${ chat.id }`
+		operators.emit( 'close', chat, room_name, action.operator )
+		operators.emit( 'message', chat, action.operator, merge( makeEventMessage( 'chat closed', chat.id ), {
+			meta: { event_type: 'close', by: action.operator }
+		} ) )
+	}
+
+	const handleSetChatOperator = ( action ) => {
+		let { operator, chat_id } = action
+		let chat = getChat( action.chat_id, store.getState() )
+		events.emit( 'found', chat, operator )
+		operators.emit( 'message', chat, operator, merge( makeEventMessage( 'operator assigned', chat_id ), {
+			meta: { operator, event_type: 'assigned' }
+		} ) )
+	}
+
+	const handleRecoverChats = ( action ) => {
+		let { operator, socket } = action
+		let chats = getOperatorAbandonedChats( operator.id, store.getState() )
+		// TODO: should this time out?
+		operators.emit( 'recover', { user: operator, socket: socket, room: `operators/${ operator.id }` }, chats, () => {
+			store.dispatch( setChatsRecovered( map( lensProp( 'id' ), chats ) ) )
+		} )
+	}
+
+	const handleReassignChats = ( action ) => {
+		const { operator, socket } = action
+		debug( 'reassign', operator )
+		const chats = getChatsForOperator( operator.id, store.getState() )
+		debug( 'found existing chats, reassign:', operator, chats )
+		operators.emit( 'reassign', operator, socket, chats )
+	}
+
+	const handleBroadcastChats = ( action ) => {
+		debug( 'state', getChats( store.getState() ) )
+		action.socket.emit( 'chats', getChats( store.getState() ) )
+	}
+
+	const handleTransferChat = ( action ) => {
+		debug( 'time to do the transfer dance', store.getState() )
+		const { chat_id, to, from } = action
+		const chat = getChat( chat_id, store.getState() )
+		timeout( new Promise( ( resolve, reject ) => {
+			operators.emit( 'message', chat, from, merge( makeEventMessage( 'chat transferred', chat_id ), {
+				meta: { from, to, event_type: 'transfer' }
+			} ) )
+			operators.emit( 'transfer', chat, from, to, asCallback( resolve, reject ) )
+		} ), events._timeout )
+		.then(
+			id => {
+				events.emit( 'transfer', chat, id )
+			},
+			e => {
+				debug( 'failed to transfer chat', e )
+				store.dispatch( setChatMissed( chat.id, e ) )
+			}
+		)
+	}
+
+	const handleAssignChat = ( action ) => {
+		const { chat } = action
+		const customer_room_name = `customers/${chat.id}`
+		debug( 'attempting to assign chat' )
+		// events.emit( 'chat.status', STATUS_ASSIGNING, chatToAssign )
+		timeout( new Promise( ( resolve, reject ) => {
+			operators.emit( 'assign', chat, customer_room_name, asCallback( resolve, reject ) )
+		} ), events._timeout )
+		.then(
+			( op ) => {
+				store.dispatch( setChatOperator( chat.id, op ) )
+			},
+			e => store.dispatch( setChatMissed( chat.id, e ) )
+		)
+	}
+
+	const handleAssignNextChat = () => {
+		if ( isAssigningChat( store.getState() ) ) {
+			debug( 'aready assigning chat, wait until complete' )
+			return
+		}
+
+		if ( haveMissedChat( store.getState() ) ) {
+			debug( 'assign missed chat' )
+			store.dispatch( assignChat( getNextMissedChat( store.getState() ) ) )
+		}
+
+		if ( havePendingChat( store.getState() ) ) {
+			debug( 'assign pending chat' )
+			store.dispatch( assignChat( getNextPendingChat( store.getState() ) ) )
+			return
+		}
+
+		debug( 'no chats to assign' )
+	}
+
 	return next => action => {
 		debug( 'received', action.type )
-		const prevState = store.getState()
+		const lastState = store.getState()
 		const result = next( action )
 		switch ( action.type ) {
 			case RECEIVE_CUSTOMER_MESSAGE:
-				debug( 'see if we should assign?', getChatStatus( action.chat.id, store.getState() ) )
-				// select status of chat
-				if ( isChatStatusNew( action.chat.id, store.getState() ) ) {
-					store.dispatch( insertPendingChat( action.chat ) )
-					break
-				}
-				debug( 'chat exists time to make sure someone is home' )
+				handleReceiveCustomerMessage( action )
 				break
 			// tried to assign but couldn't
 			case SET_CHAT_MISSED:
-				let previousStatus = getChatStatus( action.chat_id, prevState )
-				let missedChat = getChat( action.chat_id, store.getState() )
-				if ( previousStatus !== STATUS_MISSED ) {
-					debug( 'chat missed', action.chat_id, previousStatus, action.error )
-					events.emit( 'miss', action.error, missedChat, previousStatus )
-				}
+				handleSetChatMissed( action, lastState )
 				break;
 			case CLOSE_CHAT:
-				let closedChat = getChat( action.chat_id, prevState )
-				const room_name = `customers/${ closedChat.id }`
-				operators.emit( 'close', closedChat, room_name, action.operator )
-				operators.emit( 'message', closedChat, action.operator, merge( makeEventMessage( 'chat closed', closedChat.id ), {
-					meta: { event_type: 'close', by: action.operator }
-				} ) )
+				handleCloseChat( action, lastState )
 				break
 			case SET_CHAT_OPERATOR:
-				let { operator, chat_id } = action
-				let chatToUpdate = getChat( action.chat_id, store.getState() )
-				events.emit( 'found', chatToUpdate, operator )
-				operators.emit( 'message', chatToUpdate, operator, merge( makeEventMessage( 'operator assigned', chat_id ), {
-					meta: { operator, event_type: 'assigned' }
-				} ) )
+				handleSetChatOperator( action )
 				break
 			case RECOVER_CHATS:
-				let { operator: recoverOperator, socket: recoverSocket } = action
-				let abandonedChats = getOperatorAbandonedChats( recoverOperator.id, store.getState() )
-				// TODO: should this time out?
-				operators.emit( 'recover', { user: recoverOperator, socket: recoverSocket, room: `operators/${ recoverOperator.id }` }, abandonedChats, () => {
-					store.dispatch( setChatsRecovered( map( lensProp( 'id' ), abandonedChats ) ) )
-				} )
+				handleRecoverChats( action )
 				break;
 			case REASSIGN_CHATS:
-				const { operator: reassignOperator, socket } = action
-				debug( 'reassign', reassignOperator )
-				const chats = getChatsForOperator( reassignOperator.id, store.getState() )
-				debug( 'found existing chats, reassign:', reassignOperator, chats )
-				operators.emit( 'reassign', reassignOperator, socket, chats )
+				handleReassignChats( action )
 				break;
 			case BROADCAST_CHATS:
-				debug( 'state', getChats( store.getState() ) )
-				action.socket.emit( 'chats', getChats( store.getState() ) )
+				handleBroadcastChats( action )
 				break
 			case TRANSFER_CHAT:
-				debug( 'time to do the transfer dance', store.getState() )
-				const { chat_id: transfer_chat_id, to, from } = action
-				const toTransferChat = getChat( transfer_chat_id, store.getState() )
-				timeout( new Promise( ( resolve, reject ) => {
-					operators.emit( 'message', toTransferChat, from, merge( makeEventMessage( 'chat transferred', transfer_chat_id.id ), {
-						meta: { from, to, event_type: 'transfer' }
-					} ) )
-					operators.emit( 'transfer', toTransferChat, from, to, asCallback( resolve, reject ) )
-				} ), events._timeout )
-				.then(
-					id => {
-						events.emit( 'transfer', toTransferChat, id )
-					},
-					e => {
-						debug( 'failed to transfer chat', e )
-						store.dispatch( setChatMissed( toTransferChat.id, e ) )
-					}
-				)
+				handleTransferChat( action )
 				break
 			case ASSIGN_CHAT:
-				const chatToAssign = action.chat
-				const customer_room_name = `customers/${chatToAssign.id}`
-				debug( 'attempting to assign chat' )
-				// events.emit( 'chat.status', STATUS_ASSIGNING, chatToAssign )
-				timeout( new Promise( ( resolve, reject ) => {
-					operators.emit( 'assign', chatToAssign, customer_room_name, asCallback( resolve, reject ) )
-				} ), events._timeout )
-				.then(
-					( op ) => {
-						store.dispatch( setChatOperator( chatToAssign.id, op ) )
-					},
-					e => store.dispatch( setChatMissed( chatToAssign.id, e ) )
-				)
+				handleAssignChat( action )
 				break
 			case ASSIGN_NEXT_CHAT:
-				if ( isAssigningChat( store.getState() ) ) {
-					debug( 'aready assigning chat, wait until complete' )
-					break;
-				}
-
-				if ( haveMissedChat( store.getState() ) ) {
-					debug( 'assign missed chat' )
-					store.dispatch( assignChat( getNextMissedChat( store.getState() ) ) )
-				}
-
-				if ( havePendingChat( store.getState() ) ) {
-					debug( 'assign pending chat' )
-					store.dispatch( assignChat( getNextPendingChat( store.getState() ) ) )
-					break;
-				}
-
-				debug( 'no chats to assign' )
-				break
-			case ASSIGN_MISSED_CHAT:
-				store.dispatch( assignNextChat() )
+				handleAssignNextChat( action )
 				break
 			case SET_CHAT_STATUS:
 				debug( SET_CHAT_STATUS, action.chat.id, action.status )
