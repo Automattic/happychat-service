85bf0cff762c3ce3c570091b72bd20b85f1f4233
Robert Collins <robert@automattic.com>
2016-10-31 12:27:45 -0700

    Service creates the store and event emitter interfaces
    Event emitters and store are now injected into the separate components

--- src/service.js
+++ src/service.js
@@ -1,19 +1,41 @@
+import { EventEmitter } from 'events'
 import IO from 'socket.io'
 import customer from './customer'
 import agent from './agent'
 import operator from './operator'
 import buildController from './controller'
+import chatlist from 'chat-list'
+import createStore from 'store'
 
 const debug = require( 'debug' )( 'happychat:main' )
 
 export default ( server, { customerAuthenticator, agentAuthenticator, operatorAuthenticator } ) => {
 	debug( 'configuring socket.io server' )
-	const io = new IO( server )
-	const agents = agent( io.of( '/agent' ) ).on( 'connection', agentAuthenticator )
-	const customers = customer( io.of( '/customer' ) ).on( 'connection', customerAuthenticator )
-	const operators = operator( io.of( '/operator' ) ).on( 'connection', operatorAuthenticator )
 
-	const controller = buildController( { customers, agents, operators } )
+	const io = new IO( server )
+
+	const operatorEvents = new EventEmitter()
+	const customerEvents = new EventEmitter()
+	const chatlistEvents = new EventEmitter()
+
+	const store = createStore( {
+		io,
+		operators: operatorEvents,
+		customers: customerEvents,
+		chatlist: chatlistEvents
+	} );
+
+	const agents = agent( io.of( '/agent' ) ).on( 'connection', agentAuthenticator )
+	const customers = customer( io.of( '/customer' ), customerEvents ).on( 'connection', customerAuthenticator )
+	const operators = operator( io.of( '/operator' ), operatorEvents, store ).on( 'connection', operatorAuthenticator )
+
+	const controller = buildController( {
+		customers,
+		agents,
+		operators,
+		chatlist: chatlist( { store, events: chatlistEvents } ),
+		store
+	} )
 
 	return { io, agents, customers, operators, controller }
 }
