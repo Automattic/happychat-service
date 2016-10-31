88e7fb2dc7fb82b79c580b4ffe942a60cde8c88e
Robert Collins <robert@automattic.com>
2016-10-31 12:29:42 -0700

    Chatlist now accepts injected store and event emitter

--- src/chat-list/index.js
+++ src/chat-list/index.js
@@ -1,18 +1,8 @@
-import { EventEmitter } from 'events'
-import { createStore, applyMiddleware } from 'redux'
 import {
 	getChats
 } from './selectors'
-import reducer from './reducer'
-import middleware from './middleware'
 
-export default ( { customers, operators, timeout = 1000, customerDisconnectTimeout = 90000, state = undefined } ) => {
-	const events = new EventEmitter()
-	events._timeout = timeout
-	events._customerDisconnectTimeout = customerDisconnectTimeout
-	const io_middleware = middleware( { customers, operators, events } )
-	const store = createStore( reducer, state, applyMiddleware( io_middleware ) )
-
+export default ( { store, events } ) => {
 	return {
 		findAllOpenChats: () => getChats( store.getState() ),
 		on: events.on.bind( events ),
