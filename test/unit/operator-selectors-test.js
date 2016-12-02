8f571ecc2a008d028f800118477ab568b0f9ee9f
Robert Collins <robert@automattic.com>
2016-12-02 10:30:10 -0800

    comments on test assertions

--- test/unit/operator-selectors-test.js
+++ test/unit/operator-selectors-test.js
@@ -3,6 +3,7 @@ import { getAvailableCapacity, STATUS_AVAILABLE } from 'operator/selectors'
 
 describe( 'Operator selectors', () => {
 	it( 'should calculate available capacity', () => {
+		// total of all operators online: true and status == STATUS_AVAILABLE
 		equal(
 			getAvailableCapacity( { operators: { identities: {
 				a: { id: 'a', online: true, status: STATUS_AVAILABLE, load: 1, capacity: 4 },
@@ -11,6 +12,7 @@ describe( 'Operator selectors', () => {
 			4
 		)
 
+		// exludes operators with online: false
 		equal(
 			getAvailableCapacity( { operators: { identities: {
 				a: { id: 'a', online: false, status: STATUS_AVAILABLE, load: 1, capacity: 4 },
@@ -18,6 +20,7 @@ describe( 'Operator selectors', () => {
 			0
 		)
 
+		// exclude operators with status != STATUS_AVAILABLE
 		equal(
 			getAvailableCapacity( { operators: { identities: {
 				a: { id: 'a', online: false, status: 'other', load: 1, capacity: 4 },
