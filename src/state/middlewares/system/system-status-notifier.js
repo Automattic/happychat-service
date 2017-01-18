4d1061a780dcee7343c0d948ebd02090f7ec1c81
Robert Collins <robert@automattic.com>
2017-01-17 16:51:53 -0800

    prevents calculating change in system status during notice action

--- src/state/middlewares/system/system-status-notifier.js
+++ src/state/middlewares/system/system-status-notifier.js
@@ -6,8 +6,12 @@ import {
 
 import { notifySystemStatusChange } from '../../chatlist/actions'
 import { getAvailableLocales } from '../../operator/selectors'
+import { NOTIFY_SYSTEM_STATUS_CHANGE } from '../../action-types'
 
 const notifySystemStatus = ( { getState, dispatch } ) => next => action => {
+	if ( action.type === NOTIFY_SYSTEM_STATUS_CHANGE ) {
+		return next( action )
+	}
 	const previous = getAvailableLocales( getState() )
 	const result = next( action )
 	const current = getAvailableLocales( getState() )
