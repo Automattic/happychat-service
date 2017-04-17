95592ff09a8e01f3ea3448997e9dde413bc42ed0
Robert Collins <robert@automattic.com>
2017-04-17 13:20:51 -0700

    System status notifiter is now throttled for performance improvements

--- src/state/middlewares/system/system-status-notifier.js
+++ src/state/middlewares/system/system-status-notifier.js
@@ -3,22 +3,29 @@ import {
 	not,
 	symmetricDifference
 } from 'ramda'
+import { throttle } from 'lodash'
 
 import { notifySystemStatusChange } from '../../chatlist/actions'
 import { getAvailableLocales } from '../../operator/selectors'
 import { NOTIFY_SYSTEM_STATUS_CHANGE } from '../../action-types'
 
-const notifySystemStatus = ( { getState, dispatch } ) => next => action => {
-	if ( action.type === NOTIFY_SYSTEM_STATUS_CHANGE ) {
-		return next( action )
+const notifySystemStatus = ( { getState, dispatch } ) => {
+	let current = getAvailableLocales( getState() )
+	let check = throttle( () => {
+		const updated = getAvailableLocales( getState() )
+		if ( not( isEmpty( symmetricDifference( current, updated ) ) ) ) {
+			dispatch( notifySystemStatusChange( updated ) )
+		}
+		current = updated
+	}, 1000, { leading: true } )
+	return next => action => {
+		if ( action.type === NOTIFY_SYSTEM_STATUS_CHANGE ) {
+			return next( action )
+		}
+		const result = next( action )
+		check()
+		return result;
 	}
-	const previous = getAvailableLocales( getState() )
-	const result = next( action )
-	const current = getAvailableLocales( getState() )
-	if ( not( isEmpty( symmetricDifference( previous, current ) ) ) ) {
-		dispatch( notifySystemStatusChange( current ) )
-	}
-	return result;
 }
 
 export default notifySystemStatus
