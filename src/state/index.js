14ef58aabb2b4053070e6f678651f97bd9f3ac6d
Robert Collins <robert@automattic.com>
2017-04-19 09:25:58 -0700

    semicolons

--- src/state/index.js
+++ src/state/index.js
@@ -1,20 +1,25 @@
 import delayedDispatch from 'redux-delayed-dispatch';
-import { applyMiddleware } from 'redux'
+import { applyMiddleware } from 'redux';
 
-import socketioMiddleware from './middlewares/socket-io'
-import systemMiddleware from './middlewares/system'
-import logger from './middlewares/logger'
-import { DESERIALIZE, SERIALIZE } from './action-types'
+import socketioMiddleware from './middlewares/socket-io';
+import systemMiddleware from './middlewares/system';
+import logger from './middlewares/logger';
+import { DESERIALIZE, SERIALIZE } from './action-types';
 
-export const serializeAction = () => ( { type: SERIALIZE } )
-export const deserializeAction = () => ( { type: DESERIALIZE } )
+export const serializeAction = () => ( { type: SERIALIZE } );
+export const deserializeAction = () => ( { type: DESERIALIZE } );
 
 export default ( { io, customerAuth, operatorAuth, agentAuth, messageMiddlewares = [], timeout = undefined }, middlewares = [] ) => {
 	return applyMiddleware(
+		// logs dispatches
 		logger,
+		// allows middleware to be injected
 		... middlewares,
+		// middlware for dispatching in the future and cancelling scheduled dispatches
 		delayedDispatch,
+		// enables interface for socket.io clients
 		... socketioMiddleware( { io, customerAuth, operatorAuth, agentAuth, messageMiddlewares, timeout } ),
+		// core middlewares enabling support chat
 		...systemMiddleware( messageMiddlewares, timeout ),
-	)
-}
+	);
+};
