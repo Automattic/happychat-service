e7c1a8c58f1fb14d200ae83c780201f5189e3de5
Robert Collins <robert@automattic.com>
2017-04-27 12:27:46 -0700

    Adding jsdoc

--- src/state/locales/selectors.js
+++ src/state/locales/selectors.js
@@ -2,12 +2,33 @@ import { path, compose, defaultTo, map, when, merge, mergeAll, has, values } fro
 
 const normalizeLocale = locale => locale;
 
+/**
+ * Gets the system's default locale setting.
+ *
+ * @param { Object } state - redux state
+ * @returns  { String } default locale code (e.g. "en-US")
+ */
 export const getDefaultLocale = path( [ 'locales', 'defaultLocale' ] );
+
+/**
+ * Gets the list of supported locales.
+ *
+ * @param { Object } state - redux global state
+ * @returns { String[] } list of supported locales.
+ */
 export const getSupportedLocales = compose(
 	defaultTo( [] ),
 	path( [ 'locales', 'supported' ] )
 );
 
+/**
+ * Gets the locale membership information for a specific operator.
+ *
+ * @param { String } locale - locale code
+ * @param { String } user_id - the operator's user id
+ * @param { Object } state - redux state
+ * @returns { Object } mebership details (e.g. { capacity: 1, active: false } )
+ */
 export const getLocaleMembership = ( locale, user_id, state ) => compose(
 	map( when( isNaN, 0 ) ),
 	merge( { capacity: 0, load: 0, active: false } ),
