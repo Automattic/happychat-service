05d0f964cec494a328419790bd087e365ae01dbe
Robert Collins <robert@automattic.com>
2017-05-05 10:10:45 -0700

    More middleware tests

--- test/unit/state/mock-state.js
+++ test/unit/state/mock-state.js
@@ -1,4 +1,4 @@
-import { STATUS_ASSIGNED, STATUS_CLOSED } from 'state/chatlist/reducer';
+import { STATUS_ASSIGNED, STATUS_CLOSED, STATUS_NEW } from 'state/chatlist/reducer';
 import groups from 'state/groups/reducer';
 import locales from 'state/locales/reducer';
 
@@ -6,11 +6,13 @@ export const onlineOperatorID = 'operator-1';
 export const transferOnlyOperatorID = 'operator-transfer';
 export const assignedChatID = 'chat-1';
 export const closedChatID = 'closed-chat-2';
+export const newChatID = 'new-chat';
 
 export const onlineOperator = { id: onlineOperatorID };
 export const transferOnlyOperator = { id: transferOnlyOperatorID };
 export const assignedChat = { id: assignedChatID, session_id: assignedChatID };
 export const closedChat = { id: closedChatID, session_id: closedChatID };
+export const newChat = { id: newChatID, session_id: newChatID };
 
 export const state = {
 	chatlist: {
@@ -29,6 +31,15 @@ export const state = {
 			onlineOperator,
 			1,
 			{ [ onlineOperatorID ]: true },
+			null,
+			null
+		],
+		[ newChatID ]: [
+			STATUS_NEW,
+			newChat,
+			null,
+			1,
+			{},
 			null,
 			null
 		]
